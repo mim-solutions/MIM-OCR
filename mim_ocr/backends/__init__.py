a2b2f4944f4cdd3daa0059dad59f1be68944b4cd
Jan Znaniecki <jan.znaniecki@mim-solutions.ai>
2023-01-31 16:26:32 +0100

    init

--- mim_ocr/backends/__init__.py
+++ mim_ocr/backends/__init__.py
@@ -2,4 +2,3 @@ from .backend import OCRBackend, OCRBackendException
 from .tesseract import TesseractBackend
 from .aws_textract import AwsTextractBackend
 from .google_vision import GCPBackend
-from .idrs import IDRSBackend
